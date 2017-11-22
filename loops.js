8de319ee38f3dcffcdcf67fb4805c3efa8a07e8a
Lindsey Trainor <ltrainor38@gmail.com>
2017-11-22 22:48:53 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,20 @@
+function forLoop(array){
+  for (let i=0; i < 25; i++){
+    if (i===1){
+      array.push("I am one strange loop")
+    }
+    else{
+    array.push(`I am ${i} strange loops`)}
+  }
+  return array
+}
+
+function whileLoop(n){
+  while(n>=0){
+    
+    n--;
+    return n;
+    
+  }
+  return "done"
+}
